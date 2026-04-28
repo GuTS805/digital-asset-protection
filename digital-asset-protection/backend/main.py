ea59a6d95cd1157eeadf6f20273c9e871e87e6c8
GuTS805 <alok020505@gmail.com>
2026-04-28 15:07:10 +0530

    allow all CORS origins to fix deployment

--- digital-asset-protection/backend/main.py
+++ digital-asset-protection/backend/main.py
@@ -27,12 +27,10 @@ app = FastAPI(
     lifespan=lifespan,
 )
 
-origins = os.getenv("CORS_ORIGINS", "http://localhost:3000").split(",")
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
